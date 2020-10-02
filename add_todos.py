c6084e221a066bc8af8ed684069321bef81d984f
Artemiy Firsov <artemijfirsov@mail.ru>
2020-10-02 23:34:32 +0700

    set cache dir in API

--- add_todos.py
+++ add_todos.py
@@ -1,10 +1,6 @@
-import os
-
 import sys
 
 from Constants import constants
 from Services.UnofficialNotionAPI import NotionAPI
-
-os.environ["NOTION_DATA_DIR"] = constants.NotionCachePath
 
 NotionAPI().add_todos(constants.ShoppingListURL, sys.argv[1].split(" и "))
